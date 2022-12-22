300a316a2a0ca23e4d029c710854292afd58c517
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-22 14:35:00 +0530

    js1

---
+++ scripts1.js
@@ -0,0 +1,23 @@
+// function printts(){
+//     var name = "Sgtbiomit_delgih";
+
+//     console.log(name);
+// }
+
+//printts();
+
+
+// function print3(){
+//     var name = "sgtbimit";
+
+//     setTimeout(function(){
+//         console.log(name);
+//     }, 1000);
+// }
+
+// print3();
+
+var a = 10;
+setTimeout(function(){
+console.log(a);
+},1000);
